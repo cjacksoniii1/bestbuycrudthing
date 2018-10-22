1a647810a9238ccf4f31d1baf311fbc85827fe69
Cory Jackson <thecoryjackson@gmail.com>
2018-10-22 16:48:43 -0500

    added Several Logic methods, program structure has been made

---
+++ ConnectionConnections/ProductRepository.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
+
+namespace doingconnectionstrings
+{
+    class ProductRepository
+    {
+        public ProductRepository()
+        { 
+
+#if DEBUG
+
+        string jsonText = File.ReadAllText("appsettings.development.json");
+        //string connStr = JObject.Parse(jsonText)["ConnectionStrings"]["DefaultConnection"].ToString();
+
+#else
+            string jsonText = File.ReadAllText("appsettings.release.json");
+#endif
+        
+        string connStr = JObject.Parse(jsonText)["ConnectionStrings"]["DefaultConnection"].ToString();
+        this.connStr = connStr;
+
+        }
+
+        private string connStr;
+
+        public void AddProduct(Product prod)    //Create
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);    //connects to MySql server
+
+            using (conn)        //uses connection, closes when finished
+            {
+                conn.Open();        //opens connections
+
+                MySqlCommand cmd = conn.CreateCommand();    //"makes the command"
+                cmd.CommandText = $"INSERT INTO products (Name, Price, CatagoryID) Values (@n, @p, @cid)";  //command text w/parameters, n, p, cid
+                cmd.Parameters.AddWithValue("n", prod.Name);    //enters parameter 'n' without injection
+                cmd.Parameters.AddWithValue("p", prod.Price);   //enters parameter 'p' without injection
+                cmd.Parameters.AddWithValue("cid", prod.CatID); //enters parameter 'cid' without injection
+                cmd.ExecuteNonQuery();
+
+            }
+
+             /*   Product product = new Product()
+                {
+                    Name = name,
+                    Price = price,
+                    CatID = id
+                };
+
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = $"INSERT INTO products (ProductID, Name, Price) VALUES ({product.Name} {product.CatID} {product.Price});";
+            cmd.ExecuteNonQuery(); */
+
+        }
+
+        public Product GetProduct(int id)                           //Reads single product
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);        //establishes connection
+
+            using (conn)            //uses connection, closes when finished
+            {
+                conn.Open();        //opens connection
+                        
+                MySqlCommand cmd = conn.CreateCommand();        //creates command
+                cmd.CommandText = "SELECT ProductID, Name, Price FROM products WHERE ProductID =" + id;     //command text
+                MySqlDataReader dataReader = cmd.ExecuteReader();       //declares 'dataReader' to actually read shit
+
+                if (dataReader.Read())      //if entered parameter ID exists...
+                {
+                    Product product = new Product()     //creates new product and returns it
+                    {
+                        Name = dataReader["Name"].ToString(),
+                        ID = (int)dataReader["ProductID"],
+                        Price = (decimal)dataReader["Price"]
+                    };
+                    return product;
+
+                }
+                else
+                    return null;
+            }
+        }
+
+        public List<Product> GetAllProducts()                       //(also Read)
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            List<Product> products = new List<Product>();
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT ProductID, Name, Price FROM products;";
+
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        ID = (int)dataReader["ProductID"],
+                        Name = dataReader["Name"].ToString(),
+                        Price = (decimal)dataReader["Price"]
+
+                    };
+
+                    products.Add(product);
+                }
+            }
+                return products;
+        }
+        public int UpdateProduct(Product prod)                 //update
+        {
+            var conn = new MySqlConnection(connStr);
+
+            using (conn)
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "UPDATE products SET Name = @n, Price = @p, CategoryId = @cID " +
+                                    "WHERE ProductId = @pID;";
+                cmd.Parameters.AddWithValue("n", prod.Name);
+                cmd.Parameters.AddWithValue("p", prod.Price);
+                cmd.Parameters.AddWithValue("cID", prod.CatID);
+                cmd.Parameters.AddWithValue("pID", prod.ID);
+                return cmd.ExecuteNonQuery();
+            }
+
+        }
+        public int DeleteProduct(Product prod)             //delete
+        {
+            var conn = new MySqlConnection(connStr);
+
+            using (conn)
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM products WHERE ProductID = @id;";
+                cmd.Parameters.AddWithValue("id", prod.ID);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }   
+}
+        
+
+    
---
+++ ConnectionConnections/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace doingconnectionstrings
+{
+    class Program
+    {
+        private static ProductRepository repo;
+
+        static void Main(string[] args)
+        {
+            StartProgram();
+        }
+        static void StartProgram()
+        {
+            Console.WriteLine("Building Repository...");
+            repo = new ProductRepository();
+            Console.WriteLine("Listing Products...");
+            List<Product> products = repo.GetAllProducts();
+
+            Console.WriteLine("Program Ready:");
+            Console.WriteLine("Please Enter a valid command or type \"end\"");
+            Console.WriteLine("*Create* | *Update* | *Read* | *Delete* ");
+
+            string response = "";
+            while (true)
+            {
+                response = Console.ReadLine();
+
+                if (response.ToLower() == "create")
+                    CreateLogic();
+                if (response.ToLower() == "update")
+                    UpdateLogic();        //**TODO** make UpdateLog function
+                if (response.ToLower() == "read")
+                    ReadLogic();          //**TODO** make ReadLog function
+                if (response.ToLower() == "delete")
+                    DeleteLogic();        //**TODO** make DeleteLog function
+                if (response.ToLower() == "end")
+                    break;
+                else
+                {
+                    Console.WriteLine("Please give a valid command");
+                    Console.WriteLine("*Create* | *Update* | *Read* | *Delete* ");
+                }
+            }
+        }
+        static void CreateLogic()
+        {
+            Console.WriteLine("Creating new entry...");
+
+            Product prod = new Product()
+            {
+                Name = GetString(),
+                Price = GetPrice(),
+                CatID = GetInteger()
+            };
+
+            Console.WriteLine("Adding new product...");
+            repo.AddProduct(prod);
+            Console.WriteLine("Succses!");
+
+        }
+        static void UpdateLogic()
+        {
+        }
+        static void ReadLogic()
+        {
+        }
+        static void DeleteLogic()
+        { }
+        private static string GetString()
+        {
+            string answer = Console.ReadLine();
+            if (answer.Length < 3)
+            {
+                Console.WriteLine("Please enter a name that is at least 3 characters long");
+                answer = GetString();
+            }
+            return answer;
+        }
+        private static decimal GetPrice()
+        {
+            string answer = Console.ReadLine();
+            decimal number = 0;
+            if (Decimal.TryParse(answer, out number))
+                return number;
+
+            Console.WriteLine("Please enter valid input (stored as decimal value)");
+            return GetPrice();
+        }
+        private static int GetInteger()
+        {
+            string answer = Console.ReadLine();
+            int number = 0;
+            if (Int32.TryParse(answer, out number))
+                return number;
+
+            Console.WriteLine("Please enter a non-negative, non-zero integer.");
+            return GetInteger();
+        }
+               
+    }
+}
